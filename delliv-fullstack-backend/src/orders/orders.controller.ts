e05c8ab852e6489174c7f3ff3e0801ecb131a753
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-21 23:00:56 -0300

    add jwtauthguard

--- delliv-fullstack-backend/src/orders/orders.controller.ts
+++ delliv-fullstack-backend/src/orders/orders.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { Order } from './order.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('orders')
+@UseGuards(JwtAuthGuard)
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
