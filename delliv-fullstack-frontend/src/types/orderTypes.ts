90209e32792793aa40520a897c11404f385d4366
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 18:59:06 -0300

    refactor(type): nomenclature and interface

--- delliv-fullstack-frontend/src/types/orderTypes.ts
+++ delliv-fullstack-frontend/src/types/orderTypes.ts
@@ -1,6 +1,30 @@
-export interface OrderType {
+export interface Order {
   id: number;
   customerName: string;
   deliveryAddress: string;
   status: string;
 }
+
+export interface OrdersState {
+  data: Order[]; 
+  status: 'idle' | 'loading' | 'succeeded' | 'failed'; 
+  error: string | null;
+}
+
+export const FETCH_ORDERS = 'FETCH_ORDERS';
+export const UPDATE_ORDER_STATUS = 'UPDATE_ORDER_STATUS';
+
+interface FetchOrdersAction {
+  type: typeof FETCH_ORDERS;
+  payload: Order[];
+}
+
+export interface UpdateOrderStatusAction {
+  type: typeof UPDATE_ORDER_STATUS;
+  payload: {
+    orderId: number;
+    newStatus: string;
+  };
+}
+
+export type OrdersActionTypes = FetchOrdersAction | UpdateOrderStatusAction;
