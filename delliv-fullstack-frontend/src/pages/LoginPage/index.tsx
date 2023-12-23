923df7a5e4c281c41a7c871b1e4e0ac498ab4899
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 18:55:37 -0300

    refactor(pages)

--- delliv-fullstack-frontend/src/pages/LoginPage/index.tsx
+++ delliv-fullstack-frontend/src/pages/LoginPage/index.tsx
@@ -1,10 +1,9 @@
 import LoginForm from "../../components/molecules/LoginForm";
-import { Flex } from "../../styles/global";
 
 export default function LoginPage(){
   return (
-    <Flex>
+    <div>
       <LoginForm />
-    </Flex>
+    </div>
   )
 }
--- delliv-fullstack-frontend/src/pages/OrderListPage/index.tsx
+++ delliv-fullstack-frontend/src/pages/OrderListPage/index.tsx
@@ -1,38 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import OrderListItem from "../../components/molecules/OrderListItem";
-import { RootState } from "../../redux/rootReducer";
-import { updateOrderStatus } from "../../redux/orderSlice";
-import StatusUpdateForm from "../../components/molecules/StatusUpdateForm";
-import { OrderType } from "../../types/orderTypes";
-import { fetchOrders } from "../../redux/actions/ordersActions";
+import OrdersList from "../../components/molecules/OrderListItem";
 
 export default function OrderListPage() {
-  const dispatch = useDispatch();
-  const orders: OrderType[] = useSelector((state: RootState) => state.orders.orders);
-
-  useEffect(() => {
-    // @ts-ignore
-    dispatch(fetchOrders());
-  }, [dispatch]);
-
-  const handleUpdateStatus = (orderId: number, newStatus: string) => {
-    // @ts-ignore
-    dispatch(updateOrderStatus({ orderId, newStatus }));
-  };
-
   return (
     <div>
-      {Array.isArray(orders) && orders.length > 0 ? (
-        orders.map((order) => (
-          <div key={order.id}>
-            <OrderListItem order={order} />
-            <StatusUpdateForm orderId={order.id} currentStatus={order.status} onUpdateStatus={handleUpdateStatus} />
-          </div>
-        ))
-      ) : (
-        <p>No orders available</p>
-      )}
+      <OrdersList  />
     </div>
   );
-}
+};
