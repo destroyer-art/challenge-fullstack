a323d86ff6b860d42f51e8170a7c14c2d630eb9d
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 14:43:47 -0300

    refactor

--- delliv-fullstack-frontend/src/index.tsx
+++ delliv-fullstack-frontend/src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
