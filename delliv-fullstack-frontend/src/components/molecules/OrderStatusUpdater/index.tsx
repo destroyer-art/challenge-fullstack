9c26db74615cfe7f259c9f7da86d4fbdc496471a
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 19:38:01 -0300

    refactor(component): `OrderStatusUpdater`

--- delliv-fullstack-frontend/src/components/molecules/OrderStatusUpdater/index.tsx
+++ delliv-fullstack-frontend/src/components/molecules/OrderStatusUpdater/index.tsx
@@ -8,12 +8,52 @@ interface OrderStatusUpdaterProps {
 }
 
 export default function OrderStatusUpdater({ order }: OrderStatusUpdaterProps) {
-  const [newStatus, setNewStatus] = useState<string>(order.status); // Ajuste o tipo de newStatus para string
+  const [newStatus, setNewStatus] = useState<string>(order.status);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleStatusChange = async (selectedStatus: string) => {
+    try {
+      setLoading(true);
+      await updateOrderStatus(String(order.id), selectedStatus);
+      setNewStatus(selectedStatus);
+    } catch (error) {
+      console.error('Erro ao atualizar status:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateOrderStatus = async (orderId: string, status: string) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        console.log(`Status atualizado para ${status} para o pedido ${orderId}`);
+        resolve();
+      }, 1000);
+    });
+  };
 
   return (
     <div>
-      <Label htmlFor={`status-select-${order.id}`}>Atualizar Status:</Label>
-      <SelectStatus orderId={order.id} currentStatus={newStatus} onChange={setNewStatus} />
+      <Label htmlFor={`status-select-${order.id}`} style={{ color: getStatusColor(newStatus) }}>Status: {` ${newStatus}`}</Label>
+      <SelectStatus
+        orderId={order.id}
+        currentStatus={newStatus}
+        onChange={handleStatusChange}
+      />
+      {loading && <p>Atualizando status...</p>}
     </div>
   );
 }
+
+const getStatusColor = (status: string): string => {
+  switch (status) {
+    case 'Concluído':
+      return '#096D25';
+    case 'Pendente':
+      return '#FF3933';
+    case 'Em andamento':
+      return '#FF8333';
+    default:
+      return 'black';
+  }
+};
