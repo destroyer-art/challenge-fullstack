67be4b9f972dc2ecafdf33f3586a5ac88ac4f1d8
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 09:07:47 -0300

    refactor: user.entity and removed salt

--- delliv-fullstack-backend/src/auth/users/user.entity.ts
+++ delliv-fullstack-backend/src/auth/users/user.entity.ts
@@ -1,21 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
-@Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
+  id?: number;
   name: string;
-
-  @Column({ unique: true })
   email: string;
-
-  @Column()
   password: string;
 
-  @BeforeInsert()
   async hashPassword() {
     this.password = await bcrypt.hash(this.password, 10);
   }
