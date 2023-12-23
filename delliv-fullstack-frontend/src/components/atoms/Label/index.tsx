e97c6eea60ddeb1ab365472d54047535a69f4048
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 19:34:56 -0300

    refactor(style): add style in component Label

--- delliv-fullstack-frontend/src/components/atoms/Label/index.tsx
+++ delliv-fullstack-frontend/src/components/atoms/Label/index.tsx
@@ -1,9 +1,12 @@
-import { StyledLabel } from "./styles";
+import React from 'react';
+import { StyledLabel } from './styles';
 
 interface LabelProps {
   htmlFor: string;
   children: React.ReactNode;
+  style?: React.CSSProperties;
 }
-export default function Label({ htmlFor, children }: LabelProps) {
-  return <StyledLabel htmlFor={htmlFor}>{children}</StyledLabel>;
+
+export default function Label({ htmlFor, children, style }: LabelProps) {
+  return <StyledLabel htmlFor={htmlFor} style={style}>{children}</StyledLabel>;
 }
--- delliv-fullstack-frontend/src/components/atoms/Label/styles.ts
+++ delliv-fullstack-frontend/src/components/atoms/Label/styles.ts
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const StyledLabel = styled.label`
   display: block;
   margin-bottom: 5px;
-  font-size: 14px;
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
 `;
