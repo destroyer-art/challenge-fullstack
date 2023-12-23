cb274547f42acf93c961107726111b9f1a3abd26
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 14:42:48 -0300

    refactor: removed

--- delliv-fullstack-frontend/src/redux/rootReducer.ts
+++ delliv-fullstack-frontend/src/redux/rootReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import orderReducer from './orderSlice';
-import authReducer from './authSlice';
 
 const rootReducer = combineReducers({
   orders: orderReducer,
-  auth: authReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- delliv-fullstack-frontend/src/redux/store.ts
+++ delliv-fullstack-frontend/src/redux/store.ts
@@ -1,11 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import orderReducer from './orderSlice';
+import authReducer from './reducers/authReducer';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
 
 const store = configureStore({
   reducer: {
     orders: orderReducer,
   },
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
