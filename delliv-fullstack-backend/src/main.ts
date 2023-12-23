5777ac292cd34d831001eecff0c8ead5dcbb215a
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 10:40:30 -0300

    refactor(main): add useGlobalPipe and ValidationPipe

--- delliv-fullstack-backend/src/main.ts
+++ delliv-fullstack-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
