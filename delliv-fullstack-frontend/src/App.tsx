23e2c75dc115c0f2a94e61a273df1a1e9b9d418c
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-23 18:48:24 -0300

    refactor(routes): clean code

--- delliv-fullstack-frontend/src/App.tsx
+++ delliv-fullstack-frontend/src/App.tsx
@@ -1,40 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import LoginForm from './components/molecules/LoginForm';
-import OrderListPage from './pages/OrderListPage';
-
-const isAuthenticated = true;
-
-interface PrivateRouteProps {
-  component: React.ComponentType<any>;
-  path: string;
-  exact?: boolean;
-}
-
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isAuthenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    }
-  />
-);
+import AppRouter from "./router/AppRouter";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <LoginForm />
-        </Route>
-
-        {/* Rota protegida */}
-        <PrivateRoute path="/orders" component={OrderListPage} />
-      </Switch>
-    </Router>
+    <AppRouter />
   );
 }
