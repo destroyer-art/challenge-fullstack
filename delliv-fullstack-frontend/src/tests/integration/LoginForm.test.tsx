36ca508f2af5c61f1775a78e5d3aafd61d6b6123
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-12-25 19:46:09 -0300

    feat(test): add test Unauthorized

--- delliv-fullstack-frontend/src/tests/integration/LoginForm.test.tsx
+++ delliv-fullstack-frontend/src/tests/integration/LoginForm.test.tsx
@@ -44,4 +44,42 @@ describe('LoginForm', () => {
 
     expect(history.location.pathname).toEqual('/');
   });
+
+  it('should render an error toast on login failure', async () => {
+    const onLoginSuccess = jest.fn();
+    const dispatchMock = jest.fn();
+    (useDispatch as jest.Mock).mockReturnValue(dispatchMock);
+
+    dispatchMock.mockRejectedValue({
+      response: {
+        data: {
+          error: 'Unauthorized',
+        },
+        status: 401,
+      },
+    });
+
+    const history = createMemoryHistory();
+    const { getByLabelText, getByTestId } = render(
+      <Router history={history}>
+        <LoginForm onLoginSuccess={onLoginSuccess} />
+      </Router>
+    );
+
+    const emailInput = getByLabelText('Email');
+    const passwordInput = getByLabelText('Password');
+    const submitButton = getByTestId('button');
+
+    fireEvent.change(emailInput, { target: { value: 'submittedEmail' } });
+    fireEvent.change(passwordInput, { target: { value: 'submittedPassword' } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(document.querySelector('.Toastify__toast--error')).toBeInTheDocument();
+      expect(dispatchMock).toHaveBeenCalledWith(
+        expect.any(Function)
+      );
+      expect(onLoginSuccess).toHaveBeenCalledTimes(0);
+    });
+  });
 });
